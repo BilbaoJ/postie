4f9f386d207faf05a5b74219afeb65f8e881fddb
BilbaoJ <jessicabilbao02@gmail.com>
2025-02-25 16:58:42 -0500

    Creating register flow

--- start/routes.ts
+++ start/routes.ts
@@ -7,6 +7,10 @@
 |
 */
 
+const RegisterController = () => import('#controllers/register_controller')
 import router from '@adonisjs/core/services/router'
 
 router.on('/').render('pages/home')
+
+router.get('/register', [RegisterController, 'create']).as('register.create')
+router.post('/register', [RegisterController, 'store']).as('register.store')
