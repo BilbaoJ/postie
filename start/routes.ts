4bbb280f8786c51ca787836e496850b0d20eedac
BilbaoJ <jessicabilbao02@gmail.com>
2025-03-04 10:42:42 -0500

    Liking and Unliking posts

--- start/routes.ts
+++ start/routes.ts
@@ -12,6 +12,7 @@ const AuthController = () => import('#controllers/auth_controller')
 const PostsController = () => import('#controllers/posts_controller')
 import router from '@adonisjs/core/services/router'
 import { middleware } from './kernel.js'
+const PostLikesController = () => import('#controllers/post_likes_controller')
 const FeedController = () => import('#controllers/feed_controller')
 
 router.get('/', [FeedController, 'index'])
@@ -28,3 +29,11 @@ router.post('/posts', [PostsController, 'store']).as('posts.store').use(middlewa
 router.get('/posts/:id/edit', [PostsController, 'edit']).as('posts.edit').use(middleware.auth())
 router.patch('/posts/:id', [PostsController, 'update']).as('posts.update').use(middleware.auth())
 router.delete('/posts/:id', [PostsController, 'destroy']).as('posts.destroy').use(middleware.auth())
+router
+  .post('/posts/:id/likes', [PostLikesController, 'store'])
+  .as('likes.store')
+  .use(middleware.auth())
+router
+  .delete('/posts/:id/likes', [PostLikesController, 'destroy'])
+  .as('likes.destroy')
+  .use(middleware.auth())
